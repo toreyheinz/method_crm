d96ad3256f72b6d068fcc987cd9716bb139df906
Torey Heinz <toreyheinz@gmail.com>
2012-12-14 11:16:23 -0500

    Cleaning up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
 require 'bundler/setup'
 require 'method_crm'
-
 require 'vcr'
+
+CONFIG = YAML.load_file(File.expand_path('../support/config.yml', __FILE__))
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each{|f| require f }
+
+RSpec.configure do |c|
+  c.extend VCR::RSpec::Macros
+end
 
 VCR.config do |c|
   c.default_cassette_options = { 
@@ -10,12 +16,4 @@ VCR.config do |c|
   }
   c.stub_with :webmock
   c.cassette_library_dir = 'spec/support/cassettes'
-end
-
-CONFIG = YAML.load_file(File.expand_path('../support/config.yml', __FILE__))
-
-Dir[File.expand_path('../support/**/*.rb', __FILE__)].each{|f| require f }
-
-RSpec.configure do |c|
-  c.extend VCR::RSpec::Macros
 end
